da054b29627a1f682457820da002e4048a35bec8
Adomas Ciplys <aciply22@student.aau.dk>
2024-03-10 16:41:15 +0100

    Initialized Repository

---
+++ generator/src/test/java/org/abcd/examples/ParLang/ParLangTest.java
@@ -0,0 +1,20 @@
+package org.abcd.examples.ParLang;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ParLangTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
