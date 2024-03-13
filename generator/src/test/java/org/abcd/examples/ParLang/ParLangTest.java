f59cdfc1615203499db9bbbfa902543874f11558
od45pv <od45PV@student.aau.dk>
2024-03-13 14:51:24 +0100

    Update ParLangTest.java

--- generator/src/test/java/org/abcd/examples/ParLang/ParLangTest.java
+++ generator/src/test/java/org/abcd/examples/ParLang/ParLangTest.java
@@ -1,20 +1,17 @@
 package org.abcd.examples.ParLang;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class ParLangTest
-{
+public class ParLangTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 }
