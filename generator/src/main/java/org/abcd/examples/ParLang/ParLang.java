ecb0c2f292add92898583fe6c7fd37c4b9e25c1c
EmilChri <113058804+EmilChri@users.noreply.github.com>
2024-03-30 17:31:17 +0100

    det virker sådan nogenlunde tror jeg

--- generator/src/main/java/org/abcd/examples/ParLang/ParLang.java
+++ generator/src/main/java/org/abcd/examples/ParLang/ParLang.java
@@ -11,6 +11,13 @@ package org.abcd.examples.ParLang;
 // import ANTLR's runtime libraries
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.misc.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParLang {
     public static void main(String[] args) throws Exception {
@@ -28,5 +35,131 @@ public class ParLang {
 
         ParseTree tree = parser.init(); // begin parsing at init rule
         System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+    }
+
+    public static boolean parseTest(String realInput){
+        // create a CharStream that reads from standard input
+        ANTLRInputStream input = new ANTLRInputStream(realInput);
+
+        // create a lexer that feeds off of input CharStream
+        ParLangLexer lexer = new ParLangLexer(input);
+
+        // create a buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // create a parser that feeds off the tokens buffer
+        ParLangParser parser = new ParLangParser(tokens);
+
+        parser.removeErrorListeners();
+        ErrorListener errListen = new ErrorListener();
+        parser.addErrorListener(errListen);
+
+        ParseTree tree = parser.init(); // begin parsing at init rule
+
+        List<SyntaxError> errors = errListen.getSyntaxErrors();
+        if (errors.size() != 0){return false;}
+        System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+        return true;
+    }
+    public static boolean lexerTest(String realInput){
+        // create a CharStream that reads from standard input
+        ANTLRInputStream input = new ANTLRInputStream(realInput);
+
+        // create a lexer that feeds off of input CharStream
+        ParLangLexer lexer = new ParLangLexer(input);
+
+        // create a buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // create a parser that feeds off the tokens buffer
+        ParLangParser parser = new ParLangParser(tokens);
+
+        parser.removeErrorListeners();
+        ErrorListener errListen = new ErrorListener();
+        parser.addErrorListener(errListen);
+
+        ParseTree tree = parser.init(); // begin parsing at init rule
+
+        List<SyntaxError> errors = errListen.getSyntaxErrors();
+        if (errors.size() != 0){return false;}
+        System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+        return true;
+    }
+}
+
+
+
+class ErrorListener extends BaseErrorListener{
+    private final List<SyntaxError> syntaxErrors = new ArrayList<>();
+
+    public ErrorListener() {
+    }
+
+    List<SyntaxError> getSyntaxErrors() {
+        return syntaxErrors;
+    }
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer,
+                            Object offendingSymbol,
+                            int line, int charPositionInLine,
+                            String msg, RecognitionException e)
+    {
+        syntaxErrors.add(new SyntaxError(recognizer, offendingSymbol, line, charPositionInLine, msg, e));
+    }
+
+    @Override
+    public String toString() {
+        return Utils.join(syntaxErrors.iterator(), "\n");
+    }
+}
+
+class SyntaxError
+{
+    private final Recognizer<?, ?> recognizer;
+    private final Object offendingSymbol;
+    private final int line;
+    private final int charPositionInLine;
+    private final String message;
+    private final RecognitionException e;
+
+    SyntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
+    {
+        this.recognizer = recognizer;
+        this.offendingSymbol = offendingSymbol;
+        this.line = line;
+        this.charPositionInLine = charPositionInLine;
+        this.message = msg;
+        this.e = e;
+    }
+
+    public Recognizer<?, ?> getRecognizer()
+    {
+        return recognizer;
+    }
+
+    public Object getOffendingSymbol()
+    {
+        return offendingSymbol;
+    }
+
+    public int getLine()
+    {
+        return line;
+    }
+
+    public int getCharPositionInLine()
+    {
+        return charPositionInLine;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public RecognitionException getException()
+    {
+        return e;
     }
 }
--- generator/src/test/java/org/abcd/examples/ParLang/ParLangTest.java
+++ generator/src/test/java/org/abcd/examples/ParLang/ParLangTest.java
@@ -1,7 +1,8 @@
 package org.abcd.examples.ParLang;
 
+import org.abcd.examples.ParLang.*;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for simple App.
@@ -10,8 +11,44 @@ public class ParLangTest {
     /**
      * Rigorous Test :-)
      */
-    @Test
+    /*@Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }*/
+
+    @Test
+    public void testArithExpParsing() {
+        String input = "main(){2 + 3 * 4;}";
+        assertTrue(ParLang.parseTest(input));
+    }
+
+    @Test
+    public void testWrongArithExpParsing() {
+        String input = "main(){ + 3 * 4;}";
+        assertFalse(ParLang.parseTest(input));
+    }
+
+    @Test
+    public void testControlStructureParsing() {
+        String input = "main(){if (x > 0) { x = x + 1; } else { x = x - 1; }}";
+        assertTrue(ParLang.parseTest(input));
+    }
+
+    @Test
+    public void testDeclarationParsing() {
+        String input = "main(){int x = 10;}";
+        assertTrue(ParLang.parseTest(input));
+    }
+
+    @Test
+    public void testBooleanExpressionParsing() {
+        String input = "main(){TRUE && (5 < 10) || FALSE;}";
+        assertTrue(ParLang.parseTest(input));
+    }
+
+    @Test
+    public void testWrongDeclarationParsing() {
+        String input = "main(){s int x = 10;}";
+        assertFalse(ParLang.parseTest(input));
     }
 }
