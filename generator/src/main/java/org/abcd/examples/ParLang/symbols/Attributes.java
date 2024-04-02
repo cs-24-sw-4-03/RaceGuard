7a6dcade7dc4121b7cd865f3e405e29f0f74b47e
Laczse <lass738a@ucholstebro.dk>
2024-04-02 15:50:26 +0200

    Added the most generic parts that C* has that also fits our project

--- generator/src/main/java/org/abcd/examples/ParLang/symbols/Attributes.java
+++ generator/src/main/java/org/abcd/examples/ParLang/symbols/Attributes.java
@@ -1,4 +1,29 @@
 package org.abcd.examples.ParLang.symbols;
 
 public class Attributes {
+
+    private String variableType;
+    private String scope;
+    private String kind; // TODO: Should find another name for this. Describes whether it is a declaration, function, class or similar
+
+
+    //C* sets scope after creation of object. TODO: Find out if there any benefit to this?
+    public Attributes(String variableType, String scope, String kind){
+        this.variableType = variableType;
+        this.scope = scope;
+        this.kind = kind;
+
+    }
+
+    public String getVariableType(){
+        return this.variableType;
+    }
+
+    public String getScope(){
+        return this.scope;
+    }
+
+    public String getKind(){
+        return this.kind;
+    }
 }
--- generator/src/main/java/org/abcd/examples/ParLang/symbols/Scope.java
+++ generator/src/main/java/org/abcd/examples/ParLang/symbols/Scope.java
@@ -1,15 +1,49 @@
 package org.abcd.examples.ParLang.symbols;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Scope {
     private String scopeName;
     private Scope parent;
 
     //The symbols in the scope
+    //TODO: Research whether Hashmaps are the best for these lists
     private HashMap<String, Attributes> symbols = new HashMap<>();
     private Map<String, Attributes> params = new LinkedHashMap<>();
 
+    //Nested scopes within the current scope
+    private List<Scope> children = new ArrayList<>();
+
+    public Scope(String scopeName, Scope parent){
+        this.scopeName = scopeName;
+        this.parent = parent;
+    }
+
+    public String getScopeName() {
+        return this.scopeName;
+    }
+
+    public Scope getParent() {
+        return this.parent;
+    }
+
+    public void addSymbol(String id, Attributes attribute){
+        this.symbols.put(id, attribute);
+    }
+    public HashMap<String, Attributes> getSymbols() {
+        return this.symbols;
+    }
+
+    public void addParams(String id, Attributes attribute) {
+        this.params.put(id, attribute);
+    }
+    public Map<String, Attributes> getParams() {
+        return this.params;
+    }
+
+
+
+    public void addChildToScope(Scope child){
+        this.children.add(child);
+    }
 }
