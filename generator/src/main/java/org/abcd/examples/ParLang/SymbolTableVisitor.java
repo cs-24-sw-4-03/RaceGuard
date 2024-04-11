b0b1e51e4536ae3ab3c69d97a7076ca4c6ed24ed
Lazcse <lassefonager@hotmail.com>
2024-04-11 12:53:57 +0200

    Changed some comments

--- generator/src/main/java/org/abcd/examples/ParLang/SymbolTableVisitor.java
+++ generator/src/main/java/org/abcd/examples/ParLang/SymbolTableVisitor.java
@@ -14,15 +14,15 @@ public class SymbolTableVisitor {
     public void visitChildren(AstNode node){
         for(AstNode child : node.getChildren()){
             child.accept(this);
-            //TODO: Talk about accept() in AstNode with the visitor group
+            //TODO: Talk about accept() in AstNode with the visitor group. Answer: Yes, probably
         }
     }
 
     //Declares a variable in the symbol table if it does not already exist
     private void declareNode(VarDclNode node){
-        if(symbolTable.lookUpSymbol(node.getId()) == null){
+        if(this.symbolTable.lookUpSymbol(node.getId()) == null){
             Attributes attributes = new Attributes(node.getType().toString(), "dcl");
-            symbolTable.insertSymbol(node.getId(), attributes);
+            this.symbolTable.insertSymbol(node.getId(), attributes);
         }
         //TODO: Find out what should be done, when trying to declare a symbol that already exists. Override?
     }
@@ -31,49 +31,49 @@ public class SymbolTableVisitor {
         declareNode(node);
     }
 
-    //TODO: Are we going to have an INodeVisitor?
-    //TODO: Speak to the others about how we identify nodes from one another
+    //TODO: Are we going to have an INodeVisitor? Answer: Yes
+    //TODO: Speak to the others about how we identify nodes from one another. Find a way to identify the nodes
     //Creates a new scope as an iteration node is a new scope and leaves it after visiting the children
     public void visit(IterationNode node){
-        symbolTable.addScope(node.getNodeHash());
+        this.symbolTable.addScope(node.getNodeHash());
         //Visits the children of the node to add the symbols to the symbol table
         this.visitChildren(node);
         //Leaves the scope after visiting the children, as the variables in the iteration node are not available outside the iteration node
-        symbolTable.leaveScope();
+        this.symbolTable.leaveScope();
     }
 
     //Creates a new scope as a select node is a new scope and leaves it after visiting the children
     public void visit(SelectNode node){
-        symbolTable.addScope(node.getNodeHash());
+        this.symbolTable.addScope(node.getNodeHash());
         //Visits the children of the node to add the symbols to the symbol table
         this.visitChildren(node);
         //Leaves the scope after visiting the children, as the variables in the select node are not available outside the select node
-        symbolTable.leaveScope();
+        this.symbolTable.leaveScope();
     }
     //Adds a method to the symbol table if it does not already exist
     public void visit(MethodDclNode node){
-        if(symbolTable.lookUpSymbol(node.getId()) == null){
+        if(this.symbolTable.lookUpSymbol(node.getId()) == null){
             Attributes attributes = new Attributes(node.getReturnType(), "method");
-            symbolTable.insertSymbol(node.getId(), attributes);
+            this.symbolTable.insertSymbol(node.getId(), attributes);
 
-            symbolTable.addScope(node.getNodeHash());
+            this.symbolTable.addScope(node.getNodeHash());
             //Visits the children of the node to add the symbols to the symbol table
             this.visitChildren(node);
             //Leaves the scope after visiting the children, as the variables in the method node are not available outside the method node
-            symbolTable.leaveScope();
+            this.symbolTable.leaveScope();
         }
     }
 
     //Adds the parameters of a method to the symbol table
     public void visit(ParametersNode node){
-        String scopeName = symbolTable.getCurrentScope().getScopeName();
+        String scopeName = this.symbolTable.getCurrentScope().getScopeName();
 
         //Iterates through the children of the node and adds them to the symbol table
         for(AstNode child: node.getChildren()){
             Attributes attributes = new Attributes(child.type, "param");
             attributes.setScope(scopeName);
-            symbolTable.insertParams(child.getId, attributes);
-            //TODO: Ask if we can have id (or similar) on ASTNode
+            this.symbolTable.insertParams(child.getId, attributes);
+            //TODO: Ask if we can have id (or similar) on ASTNode. Answer: I think it was a yes
         }
     }
 
@@ -92,7 +92,5 @@ public class SymbolTableVisitor {
     public void visit(AssignNode node){
         this.visitChildren(node);
     }
-
-
 
 }
