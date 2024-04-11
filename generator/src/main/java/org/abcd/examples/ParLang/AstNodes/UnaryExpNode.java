b0b7ae288082c8ba0f9d76204421e6deedcd14a8
EmilChri <113058804+EmilChri@users.noreply.github.com>
2024-04-11 12:55:27 +0200

    Update UnaryExpNode.java

--- generator/src/main/java/org/abcd/examples/ParLang/AstNodes/UnaryExpNode.java
+++ generator/src/main/java/org/abcd/examples/ParLang/AstNodes/UnaryExpNode.java
@@ -1,6 +1,6 @@
 package org.abcd.examples.ParLang.AstNodes;
 
-public class UnaryExpNode extends AstNode{
+public class UnaryExpNode extends ExprNode{
     private boolean isNegated = false;
 
     public  UnaryExpNode(){
