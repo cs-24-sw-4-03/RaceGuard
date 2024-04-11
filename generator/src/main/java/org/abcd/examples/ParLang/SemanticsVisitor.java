5b7c1f7fccc142f3def744af82aaecf2ac11528d
Lazcse <lassefonager@hotmail.com>
2024-04-11 15:09:11 +0200

    Implemented methods from NodeVisitor.java in SemanticsVisitor.java

--- generator/src/main/java/org/abcd/examples/ParLang/SemanticsVisitor.java
+++ generator/src/main/java/org/abcd/examples/ParLang/SemanticsVisitor.java
@@ -1,19 +1,145 @@
 package org.abcd.examples.ParLang;
 
-import org.abcd.examples.ParLang.AstNodes.AstNode;
+import org.abcd.examples.ParLang.AstNodes.*;
 import org.abcd.examples.ParLang.symbols.SymbolTable;
 
-public class SemanticsVisitor {
+public class SemanticsVisitor implements NodeVisitor {
     SymbolTable symbolTable;
 
     public SemanticsVisitor(SymbolTable symbolTable) {
         this.symbolTable = symbolTable;
     }
 
+    @Override
     public void visitChildren(AstNode node){
         for (AstNode child : node.getChildren()) {
             child.accept(this);
         }
+    }
+
+    @Override
+    public void visit(InitNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(BodyNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(IdentifierNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(ActorIdentifierNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(ParametersNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(ReturnStatementNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(SpawnActorNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(MethodCallNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(AssignNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(VarDclNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(ActorDclNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(ActorStateNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(KnowsNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(MethodDclNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(MainDclNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(SpawnDclNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(IntegerNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(DoubleNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(StringNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(ArithExprNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(WhileNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(ForNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(ArrayAccessNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(StateAccessNode node) {
+        this.visitChildren(node);
+    }
+
+    @Override
+    public void visit(KnowsAccessNode node) {
+        this.visitChildren(node);
     }
 
 
