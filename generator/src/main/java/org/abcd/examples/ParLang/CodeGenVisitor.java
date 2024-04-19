ab3eb48a7e6eea2dac639f8912c59617b405e9e4
od45pv <od45PV@student.aau.dk>
2024-04-19 11:42:59 +0200

    Update CodeGenVisitor.java

--- generator/src/main/java/org/abcd/examples/ParLang/CodeGenVisitor.java
+++ generator/src/main/java/org/abcd/examples/ParLang/CodeGenVisitor.java
@@ -48,6 +48,9 @@ public class CodeGenVisitor implements NodeVisitor {
         stringBuilder.setLength(0); // Resets string builder
         int indent = localIndent;
 
+        if (line.endsWith("}\n")){
+            indent--;
+        }
         line = line.indent(indent * 4);
 
         return line;
@@ -122,26 +125,13 @@ public class CodeGenVisitor implements NodeVisitor {
     public void visit(BodyNode node) {
         //stringBuilder.append("Body");
         stringBuilder.append("{\n");
-        System.out.println("builder1 " + stringBuilder.toString());
-        System.out.println("PRINT1" + codeOutput.toString());
-        System.out.println("ident: " + localIndent);
-        localIndent++;
         codeOutput.add(getLine());
+        localIndent++;
 
         this.visitChildren(node);
-        System.out.println("builder2 " + stringBuilder.toString());
-        System.out.println("PRINT2" + codeOutput.toString());
-        System.out.println("ident: " + localIndent);
 
-        System.out.println("builder3 " + stringBuilder.toString());
-        System.out.println("PRINT3" + codeOutput.toString());
-        System.out.println("ident: " + localIndent);
-        localIndent--;
         stringBuilder.append("}\n");
         codeOutput.add(getLine());
-        System.out.println("builder4 " + stringBuilder.toString());
-        System.out.println("PRINT4" + codeOutput.toString());
-        System.out.println("ident: " + localIndent);
     }
 
     @Override
@@ -312,6 +302,7 @@ public class CodeGenVisitor implements NodeVisitor {
         stringBuilder.append(VariableConverter(node.getType()));
         stringBuilder.append(" ");
         stringBuilder.append(node.getId());
+        codeOutput.add(getLine());
     }
 
     @Override
