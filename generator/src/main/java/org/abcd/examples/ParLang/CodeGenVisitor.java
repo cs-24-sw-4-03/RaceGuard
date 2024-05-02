9997fe0663d5c6ec5d0189ea269829485039827b
od45pv <od45PV@student.aau.dk>
2024-05-02 13:51:29 +0200

    Added visit nodes from the visitNodes interface

--- generator/src/main/java/org/abcd/examples/ParLang/CodeGenVisitor.java
+++ generator/src/main/java/org/abcd/examples/ParLang/CodeGenVisitor.java
@@ -245,6 +245,17 @@ public class CodeGenVisitor implements NodeVisitor {
         }
 
     }
+
+    @Override
+    public void visit(SelfNode node) {
+
+    }
+
+    @Override
+    public void visit(SenderNode node) {
+
+    }
+
     @Override
     public void visit(CompareExpNode node) {
         if(node.getIsParenthesized()){
