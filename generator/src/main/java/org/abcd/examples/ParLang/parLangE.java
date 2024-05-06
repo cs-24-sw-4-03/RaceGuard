4f32da5189ff8348fa6ed63c554b63846ee535e6
Andreas Valentin Pedersen <andreasvalentin@hotmail.com>
2024-05-06 13:54:29 +0200

    add enums

---
+++ generator/src/main/java/org/abcd/examples/ParLang/parLangE.java
@@ -0,0 +1,16 @@
+package org.abcd.examples.ParLang;
+
+//Used to evaluate strings in the source code. Changing these will require changes in CodeGenVisitor.
+public enum parLangE {
+    ON("on"),
+    LOCAL("local"),
+    ACTOR("Actor"),
+    KNOWS("Knows"),
+    STATE("State");
+
+    private String string;
+
+    private parLangE(String s){this.string=s;}
+
+    public String getValue(){return string;}
+}
