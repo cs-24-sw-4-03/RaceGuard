a7c9eb890a30deb012f75a8de4503c9256fb7344
Andreas Valentin Pedersen <andreasvalentin@hotmail.com>
2024-05-06 15:27:44 +0200

    Update CodeGenVisitor.java

--- generator/src/main/java/org/abcd/examples/ParLang/CodeGenVisitor.java
+++ generator/src/main/java/org/abcd/examples/ParLang/CodeGenVisitor.java
@@ -581,11 +581,6 @@ public class CodeGenVisitor implements NodeVisitor {
     }
 
     @Override
-    public void visit(SenderNode node) {
-
-    }
-
-    @Override
     public void visit(CompareExpNode node) {
         if(node.getIsParenthesized()){
             stringBuilder.append("(");
@@ -1025,5 +1020,10 @@ public class CodeGenVisitor implements NodeVisitor {
         visitChild(node.getChildren().get(1));
         stringBuilder.append("\n");
         codeOutput.add(getLine());
+    }
+
+    @Override
+    public void visit(BoolCompareNode node){
+
     }
 }
