6e27e3c005221bfb3fe08efddadeb83a9fa5c447
Laczse <lass738a@ucholstebro.dk>
2024-05-09 13:14:31 +0200

    Test for addScope, leaveScope and enterScope from SymbolTable.java created

--- generator/src/test/java/org/abcd/examples/ParLang/SymbolTableTests.java
+++ generator/src/test/java/org/abcd/examples/ParLang/SymbolTableTests.java
@@ -3,13 +3,14 @@ package org.abcd.examples.ParLang;
 import org.abcd.examples.ParLang.symbols.Scope;
 import org.abcd.examples.ParLang.symbols.SymbolTable;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SymbolTableTests {
 
-    private static SymbolTable symbolTable = new SymbolTable();
+    private SymbolTable symbolTable;
 
     private static Scope scope1 = new Scope("1");
     private static Scope scope2 = new Scope("2");
@@ -22,9 +23,6 @@ public class SymbolTableTests {
      *   Possible unit test candidates:
      *   SymbolTable:
      *   findActorParent
-     *   addScope
-     *   leaveScope
-     *   enterScope
      *   lookUpSymbolCurrentScope
      *   lookUpSymbol
      *   lookUpStateSymbol
@@ -33,8 +31,9 @@ public class SymbolTableTests {
 
 
 
-    @BeforeAll
-    public static void setUp(){
+    @BeforeEach
+    public void setUp(){
+        this.symbolTable = new SymbolTable();
         scope1.setParent(symbolTable.getCurrentScope());
         scope2.setParent(symbolTable.getCurrentScope());
         scope3.setParent(scope1);
@@ -48,25 +47,80 @@ public class SymbolTableTests {
         scope1.children.add(scope4);
         scope2.children.add(scope5);
         scope2.children.add(scope6);
+
+
+
+
     }
 
 
     //lookUpScope Tests:
 
     @Test
-    public void testLookUpScopeFound(){
+    public void lookUpScopeTestFound(){
         Scope foundScope = symbolTable.lookUpScope("6");
         assertSame(foundScope, scope6);
     }
 
     @Test
-    public void testLookUpScopeNotFound(){
+    public void lookUpScopeTestNotFound(){
         Scope foundScope = symbolTable.lookUpScope("7");
         assertNull(foundScope);
     }
 
 
     //addScope Tests:
+
+    @Test
+    public void addScopeTestCreated(){
+        Scope currentScope = symbolTable.getCurrentScope();
+        assertTrue(symbolTable.addScope("7"));
+        assertNotEquals(symbolTable.getCurrentScope(),currentScope);
+    }
+
+    @Test
+    public void addScopeTestAlreadyExists(){
+        Scope currentScope = symbolTable.getCurrentScope();
+        assertFalse(symbolTable.addScope("6"));
+        assertSame(currentScope, symbolTable.getCurrentScope());
+    }
+
+    //leaveScope Tests:
+
+    @Test
+    public void leaveScopeTestGlobal(){
+        Scope currentScope = symbolTable.getCurrentScope();
+        symbolTable.leaveScope();
+        assertSame(currentScope, symbolTable.getCurrentScope());
+    }
+
+    @Test
+    public void leaveScopeTestLeft(){
+        Scope currentScope = symbolTable.getCurrentScope();
+        symbolTable.addScope("7");
+        assertNotEquals(symbolTable.getCurrentScope(), currentScope);
+        symbolTable.leaveScope();
+        assertSame(currentScope, symbolTable.getCurrentScope());
+    }
+
+
+    //enterScope Tests:
+
+    @Test
+    public void enterScopeTestEntered(){
+        Scope currentScope = symbolTable.getCurrentScope();
+        assertTrue(symbolTable.enterScope("4"));
+        assertSame(symbolTable.getCurrentScope(), scope4);
+        assertNotEquals(symbolTable.getCurrentScope(), currentScope);
+    }
+
+    @Test
+    public void enterScopeTestNotEntered(){
+        Scope currentScope = symbolTable.getCurrentScope();
+        assertFalse(symbolTable.enterScope("7"));
+        assertSame(symbolTable.getCurrentScope(), currentScope);
+    }
+
 
 
 }
